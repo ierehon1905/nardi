040d114964594ca7b8ffef8e0090502048725a9c
Leon Minasyan <ierehon1905@gmail.com>
2023-07-02 01:07:52 +0400

    return to sveltekit hooks

--- front/runner.cjs
+++ front/runner.cjs
@@ -1,30 +1,3 @@
-const BACKEND_URL = 'http://localhost:8080';
+const app = import('./index.js');
 
-async function main() {
-	const polka = (await import('polka')).default;
-	const proxy = (await import('http-proxy-middleware')).createProxyMiddleware;
-
-	/**
-	 * @type {import('./build/handler')}
-	 */
-	const { handler } = await import('./handler.js');
-
-	const app = polka();
-
-	// proxy all requests to /api to the backend
-	app.use(
-		'/api',
-		proxy('/api', {
-			target: BACKEND_URL
-		})
-	);
-
-	app.use(handler);
-
-	app.listen(3000, (err) => {
-		if (err) throw err;
-		console.log(`> Running on localhost:3000`);
-	});
-}
-
-module.exports = main();
+module.exports = app;
--- front/src/hooks.server.ts
+++ front/src/hooks.server.ts
@@ -2,12 +2,13 @@ import type { Handle } from '@sveltejs/kit';
 /**
  * @type {import('got')}
  */
-import got from 'got';
+
 // import ""
 
 export const handle: Handle = async ({ event, resolve }) => {
 	if (event.url.pathname.startsWith('/api')) {
 		const origin = event.request.headers.get('Origin');
+		console.log(origin, event.url.origin);
 
 		// reject requests that don't come from the webapp, to avoid your proxy being abused.
 		// if (!origin || new URL(origin).origin !== event.url.origin) {
@@ -18,28 +19,11 @@ export const handle: Handle = async ({ event, resolve }) => {
 		url.hostname = 'localhost';
 		url.port = '8080';
 
-		// const response = await resolve({
-		// 	...event,
-		// 	request: new Request(url.toString(), event.request)
-		// });
-		// return response;
-		// resolve({
-		//     fetch
-		// })
-
-		// event.request.headers.delete('connection');
-		console.log('request', event.request.headers);
-
 		const response = await event.fetch(url.toString(), {
 			headers: event.request.headers
 		});
 
-		console.log('response', response.headers);
-
 		return response;
-
-		// return new Response(response.body, response);
-		// return new Response('custom response');
 	}
 
 	const response = await resolve(event);
