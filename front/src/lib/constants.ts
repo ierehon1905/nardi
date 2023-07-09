57f6175c0cde3eb4c400296e30bb8ab40cd04de1
Leon Minasyan <ierehon1905@gmail.com>
2023-07-09 04:05:53 +0400

    fix backend url

--- front/src/lib/constants.ts
+++ front/src/lib/constants.ts
@@ -1,7 +1,4 @@
 import { PUBLIC_BACKEND_HOST } from '$env/static/public';
 
-/**
- * Without scheme and trailing slash
- */
-export const BACKEND_HOST = PUBLIC_BACKEND_HOST || 'localhost:8080';
+export const BACKEND_HOST = PUBLIC_BACKEND_HOST || 'http://localhost:8080';
 export const DEBUG = false;
